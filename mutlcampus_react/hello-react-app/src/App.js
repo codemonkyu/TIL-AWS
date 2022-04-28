fb12a9a3ec8d68a61e8f238a033ea7b4c9b8515e
codemonkyu <codemonkyu@gmail.com>
2022-04-28 23:22:04 +0900

    react 4/28

---
+++ mutlcampus_react/hello-react-app/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "./App.css";
+import MyComponents from "./components/MyComponents";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <MyComponents name="ReactJS" />
+      </div>
+    );
+  }
+}
+
+export default App;
