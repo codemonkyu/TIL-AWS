cf5a02a1471363393fbba34da71f982f0f488214
codemonkyu <codemonkyu@gmail.com>
2022-05-03 10:56:15 +0900

    5/3

--- mutlcampus_react/hello-react-app/src/components/MyComponents.js
+++ mutlcampus_react/hello-react-app/src/components/MyComponents.js
@@ -1,43 +1,110 @@
 import React, { Component } from "react";
-import propTypes from "prop-types";
-
+import PropTypes from "prop-types";
+import "./MyComponents.css";
 class MyComponents extends Component {
   //생성자
-  constructor(props) {
-    super(props);
-    this.state = {
-      myvalue: 0,
-    };
-  }
+  // constructor(props) {
+  //     super(props);
+  //state 객체
+  // this.state = {
+  //     //상태변수
+  //     myValue: 0
+  // };
+  //event handler함수가 this를 사용할 수 있도록 this객체를 binding해주기
+  //    this.handleMinus = this.handleMinus.bind(this);
+  // };
+  // static defaultProps = {
+  //         name: "리액트JS"
+  //     }
+
+  //state 객체
+  state = {
+    //상태변수
+    myValue: 0,
+    message: "",
+    messages: ["Angular", "React", "Vue", "Ember"],
+    username: "",
+    isValid: false,
+  };
+  //Event Handler함수선언
+  // handleMinus() {
+  handleMinus = () => {
+    const { myValue } = this.state;
+    this.setState({ myValue: myValue - 1 });
+  };
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+  handleEnter = (e) => {
+    const { message, messages } = this.state;
+    //enter key
+    if (e.keyCode === 13) {
+      this.setState({
+        // messages: messages.concat(message),
+        messages: [...messages, message],
+        message: "",
+        isValid: true,
+      });
+      this.myUsername.focus();
+    }
+  };
+  handleDoubleClick = (index) => {
+    this.setState({
+      messages: this.state.messages.filter((val, idx) => idx !== index),
+    });
+  };
 
   render() {
     const { name, age } = this.props;
-    const { myvalue } = this.state;
+    const { myValue, message, username, isValid, messages } = this.state;
+    const { handleMinus, handleChange, handleEnter, handleDoubleClick } = this;
+    const message_list = messages.map((msg, idx) => (
+      <li key={idx} onDoubleClick={() => handleDoubleClick(idx)}>
+        {msg}
+      </li>
+    ));
 
     return (
       <div>
+        <h3>environment = {process.env.REACT_APP_MODE}</h3>
         <h3>
-          hello {name} / {age}
+          Hello {name} / {age}
         </h3>
-        <p>상태변수 myvalue = {myvalue}</p>
-        <button onClick={() => this.setState({ myvalue: myvalue + 1 })}>
+        <p>상태변수 myValue = {myValue}</p>
+        <button onClick={() => this.setState({ myValue: myValue + 1 })}>
           증가
         </button>
-        <button onClick={() => this.setState({ myvalue: myvalue - 1 })}>
-          감소
-        </button>
+        <button onClick={handleMinus}>감소</button>
+        <p>상태변수 message = {message}</p>
+        <input
+          type="text"
+          name="message"
+          value={message}
+          onChange={handleChange}
+          onKeyDown={handleEnter}
+        />
+        <button onClick={() => this.setState({ message: "" })}>초기화</button>
+        <ul>{message_list}</ul>
+        <p>상태변수 username = {username}</p>
+        <input
+          type="text"
+          name="username"
+          value={username}
+          onChange={handleChange}
+          className={isValid ? "success" : "failure"}
+          ref={(ref) => (this.myUsername = ref)}
+        />
       </div>
     );
   }
 }
-
 MyComponents.defaultProps = {
-  name: "ReactJS",
+  name: "리액트JS",
 };
-
 MyComponents.propTypes = {
-  name: propTypes.string,
-  age: propTypes.number.isRequired,
+  name: PropTypes.string,
+  age: PropTypes.number.isRequired,
 };
-
 export default MyComponents;
--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,31 +1,14 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => setToDo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (toDo === "") {
-      return;
-    }
-    // currentarray = []
-    setToDos((currentArray) => [toDo, ...currentArray]);
-    setToDo("");
-  };
-  console.log(toDos);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers").then();
+  }, []);
   return (
     <div>
-      <h1>My TO DOS ({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          onChange={onChange}
-          value={toDo}
-          text="text"
-          placeholder="write your to do...."
-        />
-        <button>add to do</button>
-      </form>
+      <h1> The coins </h1>
+      {loading ? <strong>Loading......</strong> : null}
     </div>
   );
 }
